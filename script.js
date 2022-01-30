5eb8ef62e663a2a2403d53c4d81dd9e57be3a37b
Muhfasul Alam <muhfasul.alam@gmail.com>
2022-01-30 17:18:48 -0500

    update api url

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ yes.onclick = function() {
 
 // Send Email to Database
 function sendData(){
-  fetch('https://api.mufasa.cc/subscribe', {
+  fetch('https://mufasa-cc.herokuapp.com/subscribe', {
     method: 'POST',
     body: JSON.stringify({
       email: emailVal,
